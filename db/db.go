855d165de4d3e8b2e2dc6c759e973238c6d0b2e3
MaineK00n <mainek00n.1229@gmail.com>
2024-09-26 15:01:31 +0900

    refactor: use std slices, maps package

--- db/db.go
+++ db/db.go
@@ -66,26 +66,3 @@ func newDB(dbType string) (DB, error) {
 	}
 	return nil, fmt.Errorf("Invalid database dialect, %s", dbType)
 }
-
-// IndexChunk has a starting point and an ending point for Chunk
-type IndexChunk struct {
-	From, To int
-}
-
-func chunkSlice(length int, chunkSize int) <-chan IndexChunk {
-	ch := make(chan IndexChunk)
-
-	go func() {
-		defer close(ch)
-
-		for i := 0; i < length; i += chunkSize {
-			idx := IndexChunk{i, i + chunkSize}
-			if length < idx.To {
-				idx.To = length
-			}
-			ch <- idx
-		}
-	}()
-
-	return ch
-}
